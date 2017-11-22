e8069ac310991ca7068597f12e4c30916eeed8cb
James Thacker <thacker01@gmail.com>
2017-11-22 13:17:48 -0500

    Updated find_food.py

--- SeeFood Server/find_food.py
+++ SeeFood Server/find_food.py
@@ -23,11 +23,18 @@ host = 'localhost'
 port = 4000
 s.connect((host,port))
 
+#######################################################
+####### CHANGE PATHS BELOW WHEN MOVING TO SERVER
+#######################################################
 
 ###### Initialization code - we only need to run this once and keep in memory.
 sess = tf.Session()
+#saver = tf.train.import_meta_graph('/home/james/SeeFood/CEG4110-Fall2017/saved_model/model_epoch5.ckpt.meta')
+#saver.restore(sess, tf.train.latest_checkpoint('/home/james/SeeFood/CEG4110-Fall2017/saved_model/'))
+
 saver = tf.train.import_meta_graph('/home/ec2-user/seefood-core-ai/saved_model/model_epoch5.ckpt.meta')
 saver.restore(sess, tf.train.latest_checkpoint('/home/ec2-user/seefood-core-ai/saved_model/'))
+
 graph = tf.get_default_graph()
 x_input = graph.get_tensor_by_name('Input_xn/Placeholder:0')
 keep_prob = graph.get_tensor_by_name('Placeholder:0')
@@ -36,27 +43,46 @@ class_scores = graph.get_tensor_by_name("fc8/fc8:0")
 
 # Send Server notice that the AI is ready
 f = s.makefile()
-f.write('\x01')
+f.write(struct.pack(">b", True))
 f.flush()
 
-# Loop to Analyze pictures from Server
-while(True):
+loop = True
 
-    # Receive filename of picture to analyze
-    image_path = s.recv(256)
+# Loop to Analyze pictures from Server
+while(loop):
+
+    try:
+        # Receive filename of picture to analyze
+        image_path = s.recv(256)
+
 
     # Work in RGBA space (A=alpha) since png's come in as RGBA, jpeg come in as RGB
     # so convert everything to RGBA and then to RGB.
-    image = Image.open(image_path)  # .convert('RGB')
-    image = image.resize((227, 227), Image.BILINEAR)
-    img_tensor = [np.asarray(image, dtype=np.float32)]
 
+        image = Image.open(image_path)  # .convert('RGB')
+        image = image.resize((227, 227), Image.BILINEAR)
+        img_tensor = [np.asarray(image, dtype=np.float32)]
+    except Exception:
+        break
 
     # Run the image in the model.
     scores = sess.run(class_scores, {x_input: img_tensor, keep_prob: 1.})
 
     # Calcuate AI confidence rating as a percent expressed as an integer
     confidence = int(round(max(scores[0][0], scores[0][1]) / (abs(scores[0][0]) + abs(scores[0][1])) * 100))
+
+    isFood = 0
+
+    if (scores[0][0] > scores[0][1]):
+        isFood = 1
+
+    f.write(struct.pack(">i", isFood))
+    f.write(struct.pack(">i", confidence))
+    f.flush()
+
+    # Confirm that find_food executed correctly to server
+    f.write(struct.pack(">b", True))
+    f.flush()
 
     # Create filename of results file to create
     path = image_path[0:-3]
@@ -65,21 +91,10 @@ while(True):
     # Actual Results file, binary file
     file = open(path, "wb")
 
-    # if np.argmax = 0; then the first class_score was higher, e.g., the model sees food.
-    # if np.argmax = 1; then the second class_score was higher, e.g., the model does not see food.
-    if np.argmax(scores) == 1:
-        # Write 0 to Results file if no food is detected
-        file.write(struct.pack('>i', 0))
-
-    else:
-        # Write 1 to Results file if food detected
-        file.write(struct.pack('>i', 1))
+    file.write(struct.pack('>i', isFood))
 
     # Write the AI confidence rating to the Results file
     file.write(struct.pack('>i', confidence))
     file.close()
 
-    # Confirm that find_food executed correctly to server
-    f = s.makefile()
-    f.write('\x01')
-    f.flush()
+
